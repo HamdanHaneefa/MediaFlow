b4c309e59326b459ed1c8a32b5aa25b94320aad5
HamdanHaneefa <hamdanhaneefa23@gmail.com>
2025-12-03 15:52:55 +0530

    env file added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import path from 'path';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      clientPort: 5173,
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
